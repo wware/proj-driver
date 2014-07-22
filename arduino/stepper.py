42b7263cf385e70ab4dc4ab32812efe324afdc0f
Will Ware <wware@alum.mit.edu>
2014-07-21 20:45:17 -0400

    Mock out stepper for now, make arduino a module.

--- arduino/stepper.py
+++ arduino/stepper.py
@@ -1,4 +1,9 @@
+import logging
+
+logger = logging.getLogger('Arduino stepper control')
+
 SIGN = 1
+MOCK = True
 
 
 class StepperComm:
@@ -6,39 +11,55 @@ class StepperComm:
     def __init__(self):
         from serial import Serial
         self.ser = Serial('/dev/tty.usbmodem1421', 9600, timeout=30)
-        #self._do(0)
 
-    def _do(self, num):
+    def steps(self, num):
         assert type(num) is type(3)
         self.ser.write(repr(num) + '\n')
         self.ser.readline()
 
     # 1/100th of an inch
     def down(self):
-        self._do(SIGN * 720)
+        self.steps(SIGN * 720)
         print 'down'
 
     def up(self):
-        self._do(SIGN * -720)
+        self.steps(SIGN * -720)
         print 'up'
 
     # 1/10th of an inch
     def Down(self):
-        self._do(SIGN * 7200)
+        self.steps(SIGN * 7200)
         print 'Down'
 
     def Up(self):
-        self._do(SIGN * -7200)
+        self.steps(SIGN * -7200)
         print 'Up'
 
     # 1 inch
     def wayDown(self):
-        self._do(SIGN * 72000)
+        self.steps(SIGN * 72000)
         print 'wayDown'
 
     def wayUp(self):
-        self._do(SIGN * -72000)
+        self.steps(SIGN * -72000)
         print 'wayUp'
+
+
+class MockStepperComm(StepperComm):
+    def __init__(self):
+        ch = logging.StreamHandler()
+        ch.setFormatter(
+            logging.Formatter("%(asctime)s - %(name)s - " +
+                              "%(levelname)s - %(message)s")
+        )
+        logger.addHandler(ch)
+        logger.setLevel(logging.DEBUG)
+
+    def steps(self, num):
+        logger.debug(num)
+
+if MOCK:
+    StepperComm = MockStepperComm
 
 
 def main():
